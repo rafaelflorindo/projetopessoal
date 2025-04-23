9147977bdd679ccc2a9a66cf5f91cb9e47e8721a
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2025-04-22 21:53:12 -0300

    Add files via upload

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,17 +1,23 @@
-import './style.css'
-const About = () =>{
+import React from 'react';
+import './style.css';
+import perfil from './perfilRafael.jpg'
 
-    return (
-        <div className='container'>
-            <h1 className='title'>Quem sou!!!</h1>
-            <p className='descricao'>Sou professor de programação back-End, 
-                Front-End, Mobile e Desktop, atualmente trabalho no curso Técnico 
-                em Desenvolvimento de Sistemas pela SEED e no curso de
-                 Analise e Desenvolvimento de Sistemas pela Faculdade SENAC Maringá
-            </p>
-        </div>
-        
-    );
-}
+const SobreMim = () => {
+  return (
+    <div className="sobre-mim-container">
+      <div className="sobre-mim-imagem">
+        <img src={perfil} alt="Minha foto" />
+      </div>
+      <div className="sobre-mim-texto">
+        <h2>Rafael Alves Florindo</h2>
+        <p><strong>Formação:</strong> Bacharel em Ciências da Computação - Mestre em Gestão do Conhecimento - Especialista em Desenvolvimento de Sistemas para Internet</p>
+        <p><strong>Área de atuação:</strong> Desenvolvimento Full Stack</p>
+        <p>
+          Sou professor de programação back-End, Front-End, Mobile e Desktop, atualmente trabalho no curso Técnico em Desenvolvimento de Sistemas pela SEED e no curso de Analise e Desenvolvimento de Sistemas pela Faculdade SENAC Maringá
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default SobreMim;

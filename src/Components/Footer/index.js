bea44a2e8e3201adba987ad12c8f8d9c72de1d7c
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2025-05-13 14:54:36 -0300

    Add files via upload

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,33 +1,41 @@
 import { Link } from 'react-router-dom';
-import './style.css'
+import './style.css';
 
 const Footer = () => {
     return (
         <footer>
-            <div className="footerNavegation">
-                <ul>
-                    <li><Link to="/">Início</Link></li>
-                    <li><Link to="/sobre-mim">Sobre Mim</Link></li>
-                    <li><Link to="/disciplinas">Disciplinas</Link></li>
-                    <li><Link to="/projetos">Projetos</Link></li>
-                    <li><Link to="/publicacoes">Publicações</Link></li>
-                    <li><Link to="/material-didatico">Material Didático</Link></li>
-                    <li><Link to="/contato">Contato</Link></li>
-                </ul>
-            </div>
+  <div id="footer-container">
+    <div class="footer-section">
+      <h3>Menu</h3>
+      <ul>
+      <li><Link to="/" >Início</Link></li>
+        <li><Link to="/sobre-mim" >Sobre Mim</Link></li>
+        <li><Link to="/disciplinas" >Disciplinas</Link></li>
+        <li><Link to="/projetos" >Projetos</Link></li>
+        <li><Link to="/publicacoes" >Publicações</Link></li>
+        <li><Link to="/material-didatico" >Material Didático</Link></li>
+        <li><Link to="/contato" >Contato</Link></li>
+      </ul>
+    </div>
+    <div class="footer-section">
+      <h3>Contato</h3>
+      <p>Endereço: Rua xxxxxxxxxxxxxxxxxxx</p>
+      <p>Telefone: (44) 98401-4320</p>
+      <p>Email: rafael.alvesflorindo@gmail.com</p>
+    </div>
+    <div class="footer-section social">
+      <h3>Redes Sociais</h3>
+      <a href="#">GitHub</a>
+      <a href="#">LinkedIn</a>
+      <a href="#">Lattes</a>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <p>&copy; 2025 Todos os direitos reservados</p>
+  </div>
+</footer>
 
-            <div className="footerSocials">
-                <a href="https://github.com/rafaelflorindo" target="_blank">GitHub</a>
-                <a href="https://linkedin.com/in/rafaelflorindo" target="_blank">LinkedIn</a>
-                <a href="http://lattes.cnpq.br/7246554526271622" target="_blank">Lattes</a>
-            </div>
-
-            <div className="footerCredits">
-                © 2025 Rafael Florindo. Todos os direitos reservados. Desenvolvido com React.
-            </div>
-        </footer>
-
-    )
-}
+    );
+};
 
 export default Footer;
